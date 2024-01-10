74079a2c1d7b8726e2bdd7fbf78bc9ba7c34a607
javi <fordrsmax@gmail.com>
2024-01-10 09:53:58 +0100

    edit index js , add routers

--- index.js
+++ index.js
@@ -6,13 +6,22 @@ const port = process.env.PORT || 3000;
 // const path = require('path');
 const cors = require('cors');
 
+//routers
+const userRoutes = require('./routes/users.routes')
+const loginRoutes = require('./routes/login.routes')
+
+//DB conection Startup
+const db = require('./config/db_pgsql')
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 
-//rutas de api para login
-// app.use('/users', usersRouter)
+app.use('/api/users',userRoutes)
+
+app.use('/api/login',loginRoutes)
+
 //* Serve static assets in production, must be at this location of this file
 // if (process.env.NODE_ENV === 'production') {
 //     //*Set static folder
@@ -21,11 +30,13 @@ app.use(helmet());
 //     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 //   }
 
+// dummy route
+// app.get("/", (req, res) => {
+//     res.send("Hello world!");
+//   });
+  
 app.listen(port, () => {
     console.log(`listening on port:${port}`);
   });
   
 module.exports = {app}
-
-
-
