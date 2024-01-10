bc5834a2d5caefbb0a0dc737fd926b0321b0cae1
rsmax <fordrsmax@gmail.com>
2024-01-10 13:08:31 +0100

    add pasword hash check on backend

--- controllers/login.controllers.js
+++ controllers/login.controllers.js
@@ -1,20 +1,28 @@
 const usersModels = require('../models/users.model');
 const { createToken } = require('../config/jsonWebToken');
+var bcrypt = require('bcryptjs');
 
 const login = async (req, res) => {
     try {
         const { email, password } = req.body; // tiene que llegar un hash de password
-        const user = await usersModels.findOne({ where: { email: email , password: password} });
-        if (user) {
-            const token = createToken({ email: user.email, role: user.role });
-            res.status(200)
-                .cookie('access_token', token)
-                .json({ role: user.role, cookie: token })
-                .send()
-        } else {
-            res.status(400).json({ msg: "wrong credentials" });
+        const user = await usersModels.findOne({ where: { email: email} });
+        if(user){
+            bcrypt.compare(password, user.password).then((res)=>{
+                console.log("comparacion de bycrypt",res);
+                if (res) {
+                    const token = createToken({ email: user.email, role: user.role });
+                    res.status(200)
+                        .cookie('access_token', token)
+                        .json({ role: user.role, cookie: token })
+                        .send()
+                } else {
+                    res.status(400).json({ msg: "wrong credentials invalid pasword" });
+                }
+            });
+        }else{
+            res.status(400).json({ msg: "wrong credentials user not found" });
         }
-
+        
     } catch (error) {
         res.status(400).json({ msg: error.message });
     }
