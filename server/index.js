843ed7ac1b91803b6ca53c0aff8564815ecf243e
ChristianH <Christianmendez1501@gmail.com>
2024-01-11 14:52:06 +0100

    CHG. Port Branch CICD

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const helmet = require('helmet');
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 // const path = require('path');
 const cors = require('cors');
 
