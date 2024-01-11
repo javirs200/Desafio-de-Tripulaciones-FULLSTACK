c7c7334def13629e5baab4fada1f606b7b0c5d9a
raulwad13 <rawad13@myyahoo.com>
2024-01-11 17:56:56 +0100

    end login view

--- Cliente/src/components/Main/Login/Login.jsx
+++ Cliente/src/components/Main/Login/Login.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import "./Login.css";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  return <div>Login</div>;
+  const navigate = useNavigate();
+  const handleButtonClick = () => {
+    navigate("/home");
+  };
+  return (
+
+    <div>
+      <img src="/PropiedadLogotipo-negro.png" alt="logo" className="logo" />
+      <h3 className="title_login">Iniciar sesión</h3>
+      <form>
+        <input
+          type="text"
+          className="input_name"
+          placeholder="Nombre de Usuario"
+        ></input>
+        <input
+          type="text"
+          className="input_password"
+          placeholder="Contraseña"
+        ></input>
+      </form>
+      <p>
+        ¿No tienes cuenta? <a>Registrate</a>
+      </p>
+      <button onClick={handleButtonClick} type="submit">
+        Continuar
+      </button>
+    </div>
+  );
 };
 
 export default Login;
--- Cliente/src/components/Main/Main.jsx
+++ Cliente/src/components/Main/Main.jsx
@@ -1,12 +1,19 @@
 import React from "react";
+import Login from "./Login/Login";
+import Home from "./Home/Home";
+import { Route, Routes, Navigate } from "react-router-dom";
+
+
+
 
 const Main = () => {
-  return (
-    <>
-    <Login/>
-    <Home/>
-    </>
-  );
+  return (<main>
+      <Routes>
+        <Route path="/login" element={<Login/>} />
+        <Route path="/home" element={<Home/>} />
+        <Route path="/" element={<Navigate to={"/login"} />} />
+      </Routes>
+    </main>)
 };
 
 export default Main;
