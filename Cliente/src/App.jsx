3a744c66adcd65987481959e430bf98e496729c1
raulwad13 <rawad13@myyahoo.com>
2024-01-11 13:28:54 +0100

    add folder work structure

---
+++ Cliente/src/App.jsx
@@ -0,0 +1,17 @@
+import {BrowserRouter} from "react-router-dom";
+import Main from './components/Main';
+
+function App() {
+ 
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Main/>
+      </BrowserRouter>
+
+    </div>
+  )
+}
+
+export default App;
---
+++ Cliente/src/components/Main/Register2/Register2.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Register2 = () => {
+  return <div>Register2</div>;
+};
+
+export default Register2;
