2bb5ac79befad2403e72b45c4487180bcb13163f
rsmax <fordrsmax@gmail.com>
2024-01-12 19:29:50 +0100

    add middlewares to precios routes

--- server/routes/precios.routes.js
+++ server/routes/precios.routes.js
@@ -1,10 +1,16 @@
 const preciosRouter = require("express").Router();
 const precios = require('../controllers/precios.controller');
 
+//middlewares de control de acceso a rutas protegidas
+const getAccessToken = require('../middleware/getAccessToken');
+const decodeToken = require('../middleware/decodeToken');
+const adminRoutes = require('../middleware/adminRoutes');
+const clientRoutes = require('../middleware/clientRoutes');
+
 // GET /api/precios/cias body->{sistema} retrun cias[]
-preciosRouter.get('/cias',precios.getCias);
+preciosRouter.get('/cias',getAccessToken, decodeToken, clientRoutes,precios.getCias);
 
 // GET /api/precios/fijos/productos body->{sistema,cia}  retrun productosCia[]
-preciosRouter.get('/fijos/productos',precios.getProductosFijosCia)
+preciosRouter.get('/fijos/productos',getAccessToken, decodeToken, clientRoutes,precios.getProductosFijosCia)
 
 module.exports = preciosRouter;
