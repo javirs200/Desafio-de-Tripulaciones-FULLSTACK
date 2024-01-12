96dc156fc5221ebc54e6b9c042e9fee297601ed3
javi <fordrsmax@gmail.com>
2024-01-12 11:55:40 +0100

    fix morgan init

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
-app.use(morgan)
+app.use(morgan('combined'))
 
 app.use('/api/users',userRoutes)
 
