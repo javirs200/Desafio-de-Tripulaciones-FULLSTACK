97e7b1fc0112322949db97cc42b26ee8ad016e31
javi <fordrsmax@gmail.com>
2024-01-17 11:11:28 +0100

    mokup login if breaks

--- server/controllers/login.controller.js
+++ server/controllers/login.controller.js
@@ -11,36 +11,42 @@ const login = async (req, res) => {
 
     try {
         const { email, password } = req.body;
-        const user = await usersModels.findOne({ where: { email: email} });
-        if(user){
-            bcrypt.compare(password, user.password).then((result)=>{
-                if (result) {
-                    const token = createToken({ email: user.email, role: user.rol });
-                    res.status(200)
-                        .cookie('access_token', token, { secure: true, httpOnly: true })
-                        .json({ msg: "login ok" });
-                } else {
-                    res.status(400).json({ msg: "Las credenciales proporcionadas son incorrectas" });
-                }
-            }).catch((error) => {
-                res.status(500).json({ msg: "Internal server error" , error:error });
-            });
-        }else{
+        const user = await usersModels.findOne({ where: { email: email } });
+        if (user) {
+            try {
+                bcrypt.compare(password, user.password).then((result) => {
+                    console.log('comparacion pass result ->', result)
+                    if (result) {
+                        const token = createToken({ email: user.email, role: user.rol });
+                        console.log('creacion token ok')
+                        res.status(200)
+                            .cookie('access_token', token) //{ secure: true, httpOnly: true }
+                            .json({ msg: "login ok" });
+                    } else {
+                        res.status(400).json({ msg: "Las credenciales proporcionadas son incorrectas" });
+                    }
+                }).catch((error) => {
+                    res.status(200).json({ msg: "login dummy" });
+                });
+            } catch {
+                res.status(200).json({ msg: "login dummy" });
+            }
+        } else {
             res.status(400).json({ msg: "Las credenciales proporcionadas son incorrectas" });
         }
-        
+
     } catch (error) {
-        res.status(500).json({ msg: "Internal server error " , error:error });
+        res.status(500).json({ msg: "Internal server error ", error: error });
     }
 };
 
 const logout = async (req, res) => {
     try {
         res.status(200)
-            .cookie('access_token', "", { secure: true, httpOnly: true })
+            .cookie('access_token', "")// { secure: true, httpOnly: true }
             .send();
     } catch (error) {
-        res.status(500).json({ msg: "Internal server error " , error:error });
+        res.status(500).json({ msg: "Internal server error ", error: error });
     }
 };
 
