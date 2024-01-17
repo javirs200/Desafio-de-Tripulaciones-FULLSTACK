52f7f14dc34112cd62c608c25ca678e36f27e961
raulwad13 <rawad13@myyahoo.com>
2024-01-17 17:10:01 +0100

    css branch

--- client/src/components/Main/Offer_View/Offer_View.jsx
+++ client/src/components/Main/Offer_View/Offer_View.jsx
@@ -3,11 +3,12 @@ import Nav from "../Nav/Nav";
 import search_bar from "../../../assets/img_offer/Search.svg";
 import download from "../../../assets/img_offer/download.svg";
 import trash from "../../../assets/img_offer/trash.svg";
-import filtro_propuestas from "../../../assets/img_offer/FiltroPropuestas.svg"
-import propuesta1 from "../../../assets/img_offer/PropuestaListado.svg"
-import propuesta2 from "../../../assets/img_offer/PropuestaListado1.svg"
-import propuesta3 from "../../../assets/img_offer/PropuestaListado2.svg"
-import propuesta4 from "../../../assets/img_offer/PropuestaListado3.svg"
+import filtro_propuestas from "../../../assets/img_offer/FiltroPropuestas.svg";
+import propuesta1 from "../../../assets/img_offer/PropuestaListado.svg";
+import propuesta2 from "../../../assets/img_offer/PropuestaListado1.svg";
+import propuesta3 from "../../../assets/img_offer/PropuestaListado2.svg";
+import propuesta4 from "../../../assets/img_offer/PropuestaListado3.svg";
+import selector_propuesta from "../../../assets/img_offer/SelectorPropuestas.svg";
 import "./Offer_View.css";
 
 const Offer_View = () => {
@@ -16,23 +17,19 @@ const Offer_View = () => {
       <Nav />
       <section className="search_bar">
         <img src={search_bar} alt="search" />
-        <img src={"offers"} alt="" />
+        <img className="selector" src={selector_propuesta} alt="selector" />
       </section>
       <section className="filters">
         <input type="checkbox" />
-        <img
-          className="filtro_propuestas"
-          src={filtro_propuestas}
-          alt="filtro"
-        />
-        <img src={download} alt="download" />
-        <img src={trash} alt="trash" />
+        <img src={filtro_propuestas} alt="filtro" />
+        <img className="boton" src={download} alt="download" />
+        <img className="boton" src={trash} alt="trash" />
       </section>
       <section className="offers">
-        <img src={propuesta1} alt="perfil_1" />
-        <img src={propuesta2} alt="perfil_2" />
-        <img src={propuesta3} alt="perfil_3" />
-        <img src={propuesta4} alt="perfil_4" />
+        <img className="offer1" src={propuesta1} alt="perfil_1" />
+        <img className="offer1" src={propuesta2} alt="perfil_2" />
+        <img className="offer1" src={propuesta3} alt="perfil_3" />
+        <img className="offer1" src={propuesta4} alt="perfil_4" />
       </section>
     </div>
   );
