1451465ca27bd75234a3b21f10689237ed4f33c8
unknown <you@example.com>
2024-01-23 20:01:17 +0100

    Add/CORS options at index.js

--- server/index.js
+++ server/index.js
@@ -19,9 +19,14 @@ const facturaRoutes = require('./routes/factura.routes')
 
 //DB conection Startup
 const database = require('./config/db_pgsql')
-database.connectSQL()
+database.connectSQL();
 
-app.use(cors());
+const corsOptions ={
+  origin:'https://cloudbuilds-client-folgybvrpq-ew.a.run.app/', 
+  credentials:true,           
+  optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
